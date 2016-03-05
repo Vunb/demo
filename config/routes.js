2b27b6e7951e634af0d5158de99f773e3db35655
Vunb <nhubaovu@gmail.com>
2016-03-05 20:55:06 +0700

    enable cors:true to use api/demo

--- config/routes.js
+++ config/routes.js
@@ -23,14 +23,14 @@
 module.exports.routes = {
 
   /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
+   * etc. depending on your default view engine) your home page.              *
+   *                                                                          *
+   * (Alternatively, remove this and add an `index.html` file in your         *
+   * `assets` directory)                                                      *
+   *                                                                          *
+   ***************************************************************************/
 
   //'/': {
   //  view: 'homepage'
@@ -38,18 +38,22 @@ module.exports.routes = {
 
   '/forward': 'StreamForwardController.forward',
 
-  '/api/demo': 'AJSDemoController.angularJSApiDemo',
+  '/api/demo': {
+    controller: 'AJSDemoController',
+    action: 'angularJSApiDemo',
+    cors: true
+  },
 
   '/openweathermap/:path/:subPath': 'StreamForwardController.openWeatherMap'
 
   /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the custom routes above, it   *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Custom routes here...                                                    *
+   *                                                                          *
+   * If a request to a URL doesn't match any of the custom routes above, it   *
+   * is matched against Sails route blueprints. See `config/blueprints.js`    *
+   * for configuration options and examples.                                  *
+   *                                                                          *
+   ***************************************************************************/
 
 };
